070f60a1d5e1582745a756a84bde78b6cb22cddb
Jeffrey McDonald <jffrmcdonald@gmail.com>
2021-11-12 15:26:10 -0800

    fixed indents

--- fifo.py
+++ fifo.py
@@ -5,18 +5,23 @@ def fifo_page_replacement(ref, capacity):
     page_fault = 0                          #number of page faults
     first_hold = 0                          #holds the value of the oldest page to be removed from the set
     for i in range(n):
-       if(len(s) < capacity):             #set has room for another page
-          if(ref[i] not in s):              #page is not in memory, page fault and add
+       if(len(s) < capacity): #set has room for another page
+           if (ref[i] not in s):
+               # page is not in memory, page fault and add
                s.add(ref[i])
                p.append(ref[i])
                page_fault += 1
-        else:                               #set is full
-           if(ref[i] not in s):             #page is not in memory, page fault FIFO
-               first_hold = p.pop(0)        #the value of the First (oldest) in queue
-               s.remove(first_hold)         #remove first from set by value
-               s.add(ref[i])                
-               p.append(ref[i])             
+
+       else:
+           # set is full
+           if (ref[i] not in s):  # page is not in memory, page fault FIFO
+               first_hold = p.pop(0)  # the value of the First (oldest) in queue
+               s.remove(first_hold)  # remove first from set by value
+               s.add(ref[i])
+               p.append(ref[i])
                page_fault += 1
+
+
 
     return page_fault
 
