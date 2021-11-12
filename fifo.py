afc30663c8e3d763c020763ecbc57dbf4acecf23
Tyler Yamaguchi <tmyamaguchi@cpp.edu>
2021-11-12 15:48:43 -0800

    Removed set for checking reference string

--- fifo.py
+++ fifo.py
@@ -1,20 +1,16 @@
 def fifo_page_replacement(ref, capacity):
-    p = []                                  #current page queue
-    s = set()                               #unordered set of current pages for quick check
+    p = []                                  #current page queue using a list
     n = len(ref)
     page_fault = 0                          #number of page faults
     first_hold = 0                          #holds the value of the oldest page to be removed from the set
     for i in range(n):
-       if(len(s) < capacity):             #set has room for another page
-          if(ref[i] not in s):              #page is not in memory, page fault and add
-               s.add(ref[i])
+       if(len(p) < capacity):               #queue has room for another page
+          if(ref[i] not in p):              #page is not in memory, page fault and add
                p.append(ref[i])
                page_fault += 1
-        else:                               #set is full
-           if(ref[i] not in s):             #page is not in memory, page fault FIFO
-               first_hold = p.pop(0)        #the value of the First (oldest) in queue
-               s.remove(first_hold)         #remove first from set by value
-               s.add(ref[i])                
+       else:                                #set is full
+          if(ref[i] not in p):              #page is not in memory, page fault FIFO
+               fp.pop(0)                    #pop the first (oldest) in queue
                p.append(ref[i])             
                page_fault += 1
 
